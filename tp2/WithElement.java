87e61409960517d83c8ed69561fcef40af2b172f
Joaquin Szterensus <142123612+joacoszterensus@users.noreply.github.com>
2023-09-17 16:31:18 -0300

    Update and rename With.java to WithElement.java

---
+++ tp2/WithElement.java
@@ -0,0 +1,20 @@
+package queue;
+
+import java.util.List;
+
+public class WithElement extends Type{
+	List<Object> lista;
+  
+  
+	
+	public void setElement(List<Object> lista) {
+		this.lista = lista;
+	}
+	public Object take() {
+        return lista.remove(0);
+
+    }
+    public Object head() {
+        return lista.get(0);
+    }
+}
