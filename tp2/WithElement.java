4a214d3963a9a4f4ee430fc4d8a5960c009fec47
Joaquin Szterensus <142123612+joacoszterensus@users.noreply.github.com>
2023-09-17 16:33:53 -0300

    Update WithElement.java

--- tp2/WithElement.java
+++ tp2/WithElement.java
@@ -6,15 +6,14 @@ public class WithElement extends Type{
 	List<Object> lista;
   
   
-	
+	public Object take() {
+        	return lista.remove(0);
+
+    	}
+    	public Object head() {
+        	return lista.get(0);
+   	 }
 	public void setElement(List<Object> lista) {
 		this.lista = lista;
 	}
-	public Object take() {
-        return lista.remove(0);
-
-    }
-    public Object head() {
-        return lista.get(0);
-    }
 }
