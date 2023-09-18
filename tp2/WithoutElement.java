8f9f3186249c0803237ee065272a49bdff6c2d33
Joaquin Szterensus <142123612+joacoszterensus@users.noreply.github.com>
2023-09-17 23:28:10 -0300

    Update WithoutElement.java

--- tp2/WithoutElement.java
+++ tp2/WithoutElement.java
@@ -2,16 +2,23 @@ package queue;
 
 import java.util.List;
 
-public class WithoutElement extends Type{
+public  class WithoutElement extends Type{
 	
 	public Object take() {
         throw new Error("Queue is empty");
-    	}
-    	public Object head() {
+    }
+    public Object head() {
         throw new Error("Queue is empty");
-   	 }
-	@Override
-	protected void setElement(List<Object> lista) {
+    }
 		
+	public int size() {
+		return 0;
 	}
+	public boolean isEmpty() {
+		return true;
+	}
+	public void setElement(List<Object> lista) {
+
 }
+}
+
