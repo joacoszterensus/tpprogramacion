87ce4601574acb46b688f996f920d1ed474014cf
Joaquin Szterensus <142123612+joacoszterensus@users.noreply.github.com>
2023-09-17 23:23:36 -0300

    Update Type.java

--- tp2/Type.java
+++ tp2/Type.java
@@ -3,8 +3,11 @@ package queue;
 import java.util.List;
 
 public abstract class Type  {
+	
+	public abstract int size();
 	public abstract Object take();
 	public abstract Object head();
-	protected abstract void setElement(List<Object> lista);
-}
+	public abstract boolean isEmpty();
+	public abstract void setElement(List<Object> lista);
 
+}
